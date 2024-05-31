2cec9b77e3503930047d195d2882590922145b51
Yuvraj960 <yuvrajgupta956@gmail.com>
2024-05-31 22:29:32 +0530

    Added Operators section

---
+++ Operators/starter.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a = 165;
+    // int b = 34;
+    // int c;
+    // c = a & b;
+    // printf("%d\n", c);
+    // c = a | b;
+    // printf("%d\n", c);
+    // c = a ^ b;
+    // printf("%d\n", c);
+    // c = a << 3;
+    // printf("%d\n", c);
+    // c = a >> 2;
+    // printf("%d\n", c);
+    // return 0;
+
+    // int i = 0, j = 1, k = 2, n;
+    // n = i++ || j++ || k++;
+    // printf("%d %d %d %d", n, i, j, k);
+    // return 0;
+
+    // int a = 3, b, c;
+    // b = 2 * (a++);
+    // c = 2 * (++a);
+    // printf("%d %d", b, c);
+    // return 0;
+
+    // int a = 10;
+    // int b = 20;
+    // char x = 1;
+    // char y = 0;
+    // if (a, b, x, y)
+    // {
+    //     printf("Hello");
+    // }
+    // else
+    // {
+    //     printf("World");
+    // }
+    // return 0;
+
+    // int i;
+    // if (i = 0, 2, 3)
+    // {
+    //     printf("hello");
+    // }
+    // else
+    // {
+    //     printf("C");
+    // }
+    // printf("%d", i);
+    // return 0;
+
+    // int a = 10, b = 11, c = 13, d;
+    // d = (a = c, b += a, c = a + b + c);
+    // printf("%d %d %d %d", d, a, b, c);
+    // return 0;
+
+    // int x;
+    // x = printf("A") || printf("B");
+    // printf("%d", x);
+    // x = printf("A") && printf("B");
+    // printf("%d\n\n\n", x);
+    // int res = printf("Hello World\n");
+    // printf("%d", res);
+    // return 0;
+}
